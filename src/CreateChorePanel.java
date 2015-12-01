9fa4a1866e7667a205c29da6ae2d97e404c8ea03
Patrick Severy <psevery33@yahoo.com>
2015-12-01 15:47:55 -0700

    A good start on the GUI

---
+++ src/CreateChorePanel.java
@@ -0,0 +1,108 @@
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JSlider;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+
+
+public class CreateChorePanel extends JPanel {
+	private JTextField nameTextField;
+
+	/**
+	 * Create the panel.
+	 */
+	public CreateChorePanel() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{10, 162, 9, 0, 208, 16, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		JLabel lblEnterChoreName = new JLabel("Enter Chore Name:");
+		GridBagConstraints gbc_lblEnterChoreName = new GridBagConstraints();
+		gbc_lblEnterChoreName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblEnterChoreName.gridx = 1;
+		gbc_lblEnterChoreName.gridy = 2;
+		add(lblEnterChoreName, gbc_lblEnterChoreName);
+		
+		nameTextField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.gridx = 4;
+		gbc_textField.gridy = 2;
+		add(nameTextField, gbc_textField);
+		nameTextField.setColumns(10);
+		
+		JLabel lblChooseDaysRemaining = new JLabel("Choose Days Remaining:");
+		GridBagConstraints gbc_lblChooseDaysRemaining = new GridBagConstraints();
+		gbc_lblChooseDaysRemaining.gridheight = 2;
+		gbc_lblChooseDaysRemaining.insets = new Insets(0, 0, 5, 5);
+		gbc_lblChooseDaysRemaining.gridx = 1;
+		gbc_lblChooseDaysRemaining.gridy = 3;
+		add(lblChooseDaysRemaining, gbc_lblChooseDaysRemaining);
+		
+		JSlider timeSlider = new JSlider();
+		timeSlider.setSnapToTicks(true);
+		timeSlider.setPaintTicks(true);
+		timeSlider.setPaintLabels(true);
+		timeSlider.setMaximum(7);
+		timeSlider.setMajorTickSpacing(1);
+		timeSlider.setValue(5);
+		GridBagConstraints gbc_slider_1_1 = new GridBagConstraints();
+		gbc_slider_1_1.gridheight = 2;
+		gbc_slider_1_1.insets = new Insets(0, 0, 5, 5);
+		gbc_slider_1_1.gridx = 4;
+		gbc_slider_1_1.gridy = 3;
+		add(timeSlider, gbc_slider_1_1);
+		
+		JLabel lblSelectMember = new JLabel("Select Member:");
+		GridBagConstraints gbc_lblSelectMember = new GridBagConstraints();
+		gbc_lblSelectMember.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSelectMember.gridx = 1;
+		gbc_lblSelectMember.gridy = 5;
+		add(lblSelectMember, gbc_lblSelectMember);
+		
+		JComboBox userComboBox = new JComboBox();
+		userComboBox.setModel(new DefaultComboBoxModel(new String[] {"Pat", "Anna ", "Ryan", "Tyler"}));
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.gridx = 4;
+		gbc_comboBox.gridy = 5;
+		add(userComboBox, gbc_comboBox);
+		
+		JLabel lblDeclareDifficulty = new JLabel("Declare Difficulty:");
+		GridBagConstraints gbc_lblDeclareDifficulty = new GridBagConstraints();
+		gbc_lblDeclareDifficulty.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDeclareDifficulty.gridx = 1;
+		gbc_lblDeclareDifficulty.gridy = 6;
+		add(lblDeclareDifficulty, gbc_lblDeclareDifficulty);
+		
+		JSlider slider = new JSlider();
+		slider.setValue(3);
+		slider.setSnapToTicks(true);
+		slider.setPaintTicks(true);
+		slider.setPaintLabels(true);
+		slider.setMaximum(5);
+		slider.setMajorTickSpacing(1);
+		GridBagConstraints difficultySlider = new GridBagConstraints();
+		difficultySlider.insets = new Insets(0, 0, 5, 5);
+		difficultySlider.gridx = 4;
+		difficultySlider.gridy = 6;
+		add(slider, difficultySlider);
+		
+		JButton btnSubmit = new JButton("Submit");
+		GridBagConstraints gbc_btnSubmit = new GridBagConstraints();
+		gbc_btnSubmit.gridwidth = 10;
+		gbc_btnSubmit.gridx = 0;
+		gbc_btnSubmit.gridy = 9;
+		add(btnSubmit, gbc_btnSubmit);
+
+	}
+
+}
---
+++ src/Window.java
@@ -0,0 +1,94 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.CardLayout;
+import java.awt.Color;
+import javax.swing.JTextArea;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.JLabel;
+import java.awt.Insets;
+import java.awt.Font;
+import javax.swing.JTabbedPane;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JTree;
+import javax.swing.JScrollPane;
+import javax.swing.BoxLayout;
+
+
+public class Window {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Window window = new Window();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Window() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		//main frame of the window, all panels are embedded on it
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(0, 191, 255));
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		//tabbed pane allows panels to be tabs
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBackground(new Color(255, 255, 255));
+		GridBagConstraints gbc_tabbedPane = new GridBagConstraints();
+		gbc_tabbedPane.fill = GridBagConstraints.BOTH;
+		gbc_tabbedPane.gridx = 0;
+		gbc_tabbedPane.gridy = 0;
+		frame.getContentPane().add(tabbedPane, gbc_tabbedPane);
+		
+		//main tabs at top (each tab is compased of a panel)
+		JPanel panelAdd = new CreateChorePanel();
+		tabbedPane.addTab("Add Chores", null, panelAdd, null);
+		
+		JPanel panelManage = new JPanel();
+		tabbedPane.addTab("Manage Chores", null, panelManage, null);
+		
+		//this panel is composed of another tabbed plane for the chores
+		JTabbedPane paneView = new JTabbedPane(JTabbedPane.BOTTOM);
+		paneView.setBackground(new Color(119, 136, 153));
+		tabbedPane.addTab("View Chores", null, paneView, null);
+		//these are the chore tabs
+		JPanel garbagePanel = new ViewChorePanel("Garbage", "Pat", "5", "False");
+		paneView.addTab("Garbage", null, garbagePanel, null);
+		
+		JPanel dishesPanel = new ViewChorePanel("Dishes", "Tyler", "3", "False");
+		paneView.addTab("Dishes", null, dishesPanel, null);
+		
+		JPanel vaccumPanel = new ViewChorePanel("Vaccum", "Ryan", "4", "True");
+		vaccumPanel.setBackground(new Color(0, 255, 127));
+		paneView.addTab("Vaccum", null, vaccumPanel, null);
+	}
+}

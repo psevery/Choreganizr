c9d6e4d31645be4417bb631d84020e7d7d44d16b
Tyler Bussell <Bussell.Tyler@gmail.com>
2015-12-04 12:11:55 -0700

    Made DBManager a singleton

--- src/def/DBManager.java
+++ src/def/DBManager.java
@@ -1,7 +1,6 @@
 package def;
 
 import java.util.List;
-import java.util.Set;
 
 import def.House;
 import def.User;
@@ -15,86 +14,15 @@ import org.hibernate.criterion.Restrictions;
 
 public class DBManager {
 	
-	public static void main(String[] args){
-		House house = new House();
-		House house2 = new House();
+	private static DBManager databaseManager = null;
 		
-		User user1 = new User();
-		user1.setHouse(house);
-		user1.setUserEmail("EMAIL1");
-		user1.setUserName("NAME1");
+	private DBManager() {}
 		
-		User user2 = new User();
-		user2.setHouse(house);
-		user2.setUserEmail("EMAIL2");
-		user2.setUserName("NAME2");
-		
-		Chore chore = new Chore();
-		chore.setHouse(house);
-		chore.setTitle("TITLE1");
-		chore.setUser(user1);
-		Chore chore2 = new Chore();
-		chore2.setTitle("TITLE2");
-		chore2.setHouse(house);
-		chore2.setUser(user1);
-		
-		(user1.getChores()).add(chore);
-		(user1.getChores()).add(chore2);
-		(house.getChores()).add(chore);
-		(house.getChores()).add(chore2);
-		(house.getUsers()).add(user1);
-		(house.getUsers()).add(user2);
-		(house2.getUsers()).add(user2);
-		
-		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		
-//		session.save(chore);
-//		session.save(chore2);
-//		session.save(user1);
-//		session.save(user2);
-		session.save(house);
-
-		
-//		System.out.println(house.getHouseID());
-		
-		session.getTransaction().commit();
-		
-		House result = (House) session.get(House.class, house.getHouseID());
-		
-		Chore[] blah = (result.getChores()).toArray(new Chore[0]);
-		for (Chore c: blah){
-			System.out.println("House");
-			System.out.println(c.getTitle());
+	public static synchronized DBManager getManager() {
+		if (databaseManager == null) {
+			databaseManager = new DBManager();
 		}
-		
-		User result2 = (User) session.get(User.class, user1.getUserID());
-		
-		Chore[] bleh = (result2.getChores()).toArray(new Chore[0]);
-		for (Chore b: bleh){
-			System.out.println("User");
-			System.out.println(b.getTitle());
-		}
-		
-		session.beginTransaction();
-//		for (User u: house.getUsers().toArray(new User[0])){
-//			u.setHouse(null);
-//			(u.getChores()).clear();
-//		}
-//		
-//		for (Chore u: house.getChores().toArray(new Chore[0])){
-//			u.setUser(null);
-////			session.update(u);
-//		}
-//		(house.getUsers()).clear();
-//		
-//		session.delete(house);
-		
-		session.getTransaction().commit();
-		
-		session.close();
-		sessionFactory.close();	
+		return databaseManager;
 	}
 		
 	public Long insertHouse(House house){
